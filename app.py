29c468ae7a425304299a843ffaec7f847d09340b
sbpolicar <sbpolicar@gmail.com>
2015-09-10 09:42:52 -0700

    heroku port

--- app.py
+++ app.py
@@ -65,6 +65,9 @@ def pokedex():
   # return json.dumps(moves2)
   return json.dumps(pokedex)
 
-app.run(debug=True)
+# app.run(debug=True)
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
 
 
