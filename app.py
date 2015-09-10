debe38c23836a2969c5d441ade163b7a79de70ee
sbpolicar <sbpolicar@gmail.com>
2015-09-10 10:09:36 -0700

    fuck fuck

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ def pokedex():
   # return json.dumps(moves2)
   return json.dumps(pokedex)
 
-app.run(debug=True)
-# if __name__ == "__main__":
-#   port = int(os.environ.get("PORT", 5000))
-#   app.run(host='0.0.0.0', port=port)
+# app.run(debug=True)
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
 
 
